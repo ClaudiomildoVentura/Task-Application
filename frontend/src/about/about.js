50aa25d7b51c6eb99470a77844ee3b4b52cfc268
Claudiomildo <claudiomildo@gmail.com>
2019-12-25 18:24:44 -0300

    New adjust

--- frontend/src/about/about.js
+++ frontend/src/about/about.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+
 import Header from '../template/header'
 
 export default class About extends Component {
--- frontend/src/todo/todo.js
+++ frontend/src/todo/todo.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import Header from '../template/header'
+import Form from './todoForm'
+import List from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+    handleAdd () {
+        console.log("add")
+    }
+
     render() {
         return (
             <div>
                 <Header nameTitle="Tarefas" nameSubTitle="Cadastro" />
+                <Form handleAdd={this.handleAdd} />
+                <List />
             </div>
         )
     }
